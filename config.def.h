660938f51523122abd3bf9c31a203a6b63cfb3c7
Akash Melachuri <amelachuri@gmail.com>
2022-02-13 16:10:10 -0500

    Catppuccin theme and gaps added

--- config.def.h
+++ config.def.h
@@ -3,20 +3,28 @@
 /* appearance */
 static const unsigned int borderpx = 2; /* border pixel of windows */
 static const unsigned int snap = 32;    /* snap pixel */
-static const int showbar = 1;           /* 0 means no bar */
-static const int topbar = 1;            /* 0 means bottom bar */
+static const unsigned int gappih = 10;  /* horiz inner gap between windows */
+static const unsigned int gappiv = 10;  /* vert inner gap between windows */
+static const unsigned int gappoh =
+    10; /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov =
+    10; /* vert outer gap between windows and screen edge */
+static const int smartgaps =
+    0; /* 1 means no outer gap when there is only one window */
+static const int showbar = 1; /* 0 means no bar */
+static const int topbar = 1;  /* 0 means bottom bar */
 static const char *fonts[] = {"Cozette:size=10"};
 static const char dmenufont[] = "Cozette:size=10";
-static const char col_gray1[] = "#080808";
+static const char col_gray1[] = "#1B1623";
 static const char col_gray2[] = "#444444";
 static const char col_gray3[] = "#bbbbbb";
 static const char col_gray4[] = "#eeeeee";
-static const char col_cyan[] = "#005577";
+static const char col_cyan[] = "#F0A8E4";
 static const char col_green[] = "#2BE491";
 static const char *colors[][3] = {
     /*               fg         bg         border   */
     [SchemeNorm] = {col_gray3, col_gray1, col_gray2},
-    [SchemeSel] = {col_green, col_gray1, col_green},
+    [SchemeSel] = {col_cyan, col_gray1, col_cyan},
 };
 
 /* tagging */
@@ -74,6 +82,23 @@ static Key keys[] = {
     {MODKEY, XK_p, spawn, {.v = dmenucmd}},
     {MODKEY, XK_Return, spawn, {.v = termcmd}},
     {MODKEY, XK_grave, togglescratch, {.v = scratchpadcmd}},
+    {MODKEY | Mod1Mask, XK_h, incrgaps, {.i = +1}},
+    {MODKEY | Mod1Mask, XK_l, incrgaps, {.i = -1}},
+    {MODKEY | Mod1Mask | ShiftMask, XK_h, incrogaps, {.i = +1}},
+    {MODKEY | Mod1Mask | ShiftMask, XK_l, incrogaps, {.i = -1}},
+    {MODKEY | Mod1Mask | ControlMask, XK_h, incrigaps, {.i = +1}},
+    {MODKEY | Mod1Mask | ControlMask, XK_l, incrigaps, {.i = -1}},
+    {MODKEY | Mod1Mask, XK_0, togglegaps, {0}},
+    {MODKEY | Mod1Mask | ShiftMask, XK_0, defaultgaps, {0}},
+    {MODKEY, XK_y, incrihgaps, {.i = +1}},
+    {MODKEY, XK_o, incrihgaps, {.i = -1}},
+    {MODKEY | ControlMask, XK_y, incrivgaps, {.i = +1}},
+    {MODKEY | ControlMask, XK_o, incrivgaps, {.i = -1}},
+    {MODKEY | Mod1Mask, XK_y, incrohgaps, {.i = +1}},
+    {MODKEY | Mod1Mask, XK_o, incrohgaps, {.i = -1}},
+    {MODKEY | ShiftMask, XK_y, incrovgaps, {.i = +1}},
+    {MODKEY | ShiftMask, XK_o, incrovgaps, {.i = -1}},
+
     {MODKEY, XK_b, togglebar, {0}},
     {MODKEY | ShiftMask, XK_j, rotatestack, {.i = +1}},
     {MODKEY | ShiftMask, XK_k, rotatestack, {.i = -1}},
