dbee7d47d00722a56df4d7932756f0ad99993c14
Akash Melachuri <amelachuri@gmail.com>
2022-05-15 19:35:53 -0400

    Fixed keybinding conflict

--- config.def.h
+++ config.def.h
@@ -99,7 +99,7 @@ static Key keys[] = {
     {MODKEY | ShiftMask, XK_y, incrovgaps, {.i = +1}},
     {MODKEY | ShiftMask, XK_o, incrovgaps, {.i = -1}},
 
-    {MODKEY, XK_m, togglebar, {0}},
+    {MODKEY | ShiftMask, XK_b, togglebar, {0}},
     {MODKEY | ShiftMask, XK_j, rotatestack, {.i = +1}},
     {MODKEY | ShiftMask, XK_k, rotatestack, {.i = -1}},
     {MODKEY, XK_j, focusstack, {.i = +1}},
