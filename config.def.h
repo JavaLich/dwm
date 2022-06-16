4f190a217ca6a74435e87cb0aa7b301a8d332475
Akash Melachuri <amelachuri@gmail.com>
2022-06-16 02:39:44 -0400

    Added alpha patch

--- config.def.h
+++ config.def.h
@@ -3,31 +3,37 @@
 /* appearance */
 static const unsigned int borderpx = 2; /* border pixel of windows */
 static const unsigned int snap = 32;    /* snap pixel */
-static const unsigned int gappih = 10;  /* horiz inner gap between windows */
-static const unsigned int gappiv = 10;  /* vert inner gap between windows */
+static const unsigned int gappih = 20;  /* horiz inner gap between windows */
+static const unsigned int gappiv = 20;  /* vert inner gap between windows */
 static const unsigned int gappoh =
-    10; /* horiz outer gap between windows and screen edge */
+    30; /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov =
-    10; /* vert outer gap between windows and screen edge */
+    30; /* vert outer gap between windows and screen edge */
 static const int smartgaps =
     0; /* 1 means no outer gap when there is only one window */
 static const int showbar = 1; /* 0 means no bar */
 static const int topbar = 1;  /* 0 means bottom bar */
 static const char *fonts[] = {"Cozette:size=10"};
 static const char dmenufont[] = "Cozette:size=10";
-static const char col_gray1[] = "#1B1623";
+static const char col_gray1[] = "#1f1f28";
 static const char col_gray2[] = "#444444";
-static const char col_gray3[] = "#bbbbbb";
+static const char col_gray3[] = "#dcd7ba";
 static const char col_gray4[] = "#eeeeee";
 static const char col_cyan[] = "#ffc86a";
 static const char col_green[] = "#2BE491";
+static const unsigned int baralpha = 0x00;
+static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3] = {
     /*               fg         bg         border   */
-    [SchemeNorm] = {col_gray3, col_gray1, col_gray2},
-    [SchemeSel] = {col_cyan, col_gray1, col_cyan},
+    [SchemeNorm] = {col_gray3, /* col_gray1 */ "#000000", col_gray2},
+    [SchemeSel] = {col_cyan, /* col_gray1 */ "#000000", col_cyan},
 };
+static const unsigned int alphas[][3] = {
+    /*               fg      bg        border     */
+    [SchemeNorm] = {OPAQUE, baralpha, borderalpha},
+    [SchemeSel] = {OPAQUE, baralpha, borderalpha},
+}; /* tagging */
 
-/* tagging */
 static const char *tags[] = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
 
 static const Rule rules[] = {
@@ -40,7 +46,7 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact = 0.55; /* factor of master area size [0.05..0.95] */
+static const float mfact = 0.50; /* factor of master area size [0.05..0.95] */
 static const int nmaster = 1;    /* number of clients in master area */
 static const int resizehints =
     0; /* 1 means respect size hints in tiled resizals */
